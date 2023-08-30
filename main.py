1439315f8e5fddf4144cc8dd9d8ccc400f8524b6
KuroGuiri <iotglider@gmail.com>
2023-08-30 19:23:30 +0600

    push

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-print("Hi pidarmot")
+inter = 42
 
 Name_Of_Pidor = "kleegl"
-
 print(Name_Of_Pidor+" Jenya, its fo u")
+
+print("Deus vult")
+print("Deus vult")
+print("Deus vult")
+print("Deus vult")
+print("Deus vult")
