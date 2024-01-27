54999c0f100f5bf671976dfda7a8d84518df7d03
KuroGuiri <iotglider@gmail.com>
2024-01-27 15:18:12 +0600

    new life

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-inter = 42
+def Jenya_pidor ():
+    print("Love Jenya")
 
-Name_Of_Pidor = "kleegl"
-print(Name_Of_Pidor+" Jenya, its fo u")
+Jenya_pidor ()
+Jenya_pidor ()
+Jenya_pidor ()
+Jenya_pidor ()
 
-print("Deus vult")
-print("Deus vult")
-print("Deus vult")
-print("Deus vult")
-print("Deus vult")
+def sum (par1, par2 =20):
+    return par1+par2
+
+print(" \n",sum(5))
+print(" \n",sum(5, 30))
+
+total = 0
+print(total)
+total -= 5
+print(total) if total == -5 else print("NOT")
